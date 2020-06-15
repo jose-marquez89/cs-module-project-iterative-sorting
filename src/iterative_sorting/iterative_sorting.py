064da3c9d28ec8ce0da967d6a1ae3a073dd2c3cd
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-15 15:35:11 -0500

    completes selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,15 +2,13 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
+        current = i
+        smallest = current
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest]:
+                smallest = j
+        if smallest != current:
+            arr[smallest], arr[current] = arr[current], arr[smallest]
 
     return arr
 
@@ -31,11 +29,11 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
