8e428535d678b8ad7b85eb881ac49a4ef4e50eac
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-15 18:28:49 -0500

    completes binary and linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,31 @@
-def linear_search(arr, target):
-    # Your code here
+from pdb import set_trace as bp
 
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    if len(arr) < 1:
+        return -1
 
-    # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        bp()
+        middle = (high + low) // 2
+        if arr[middle] == target:
+            return middle
+        else:
+            if target > arr[middle]:
+                low = middle + 1
+            elif target < arr[middle]:
+                high = middle - 1
 
-    return -1  # not found
+    return -1
